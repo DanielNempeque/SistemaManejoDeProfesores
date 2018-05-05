55f75af1c51ca60b9fc7133828411e6e569ae3b0
DanielNempeque <daniel.nempeque@gmail.com>
2018-05-05 17:42:22 -0500

    Añadiendo cosas

---
+++ src/java/Modelo/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Daniel Nempeque
+ */
+public class Usuario {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String usr;
+    private String pass;
+    
+}
